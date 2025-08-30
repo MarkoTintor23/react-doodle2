2fbe759f7181ab007ed064a38cb41281bc6fd40d
Marko Tintor <marko.tintor.18@singimail.rs>
2025-08-30 17:59:37 +0200

    reducers added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 import "./App.css";
 import Payment from "./Components/Payment";
 import { CURRENCIES } from "./Utils/CurrencyUtil";
+import { initialUserState, userReducer } from "./Reducers/User";
 
 export const CurrencyContext = createContext("USD");
 export const AmountContext = createContext(0);
 
 function App() {
+  const [userState, dispatch] = useReducer(userReducer, initialUserState);
   const [currency, setCurrency] = useState("USD");
   const [amount, setAmount] = useState(0);
-  const [userName, setUserName] = useState(null);
-  console.log(userName);
+
   const updateCurrency = (currency) => {
     setCurrency(currency);
   };
 
   const updateAmount = (value) => {
     setAmount(value);
+  };
+
+  const saveUser = () => {
+    if (
+      userState.userName === null ||
+      userState.userName.trim() === "" ||
+      userState.money === null ||
+      isNaN(userState.money)
+    ) {
+      return;
+    }
+    dispatch({ type: "SET_USER_CREATED", payload: true });
   };
   return (
     <div>
@@ -25,10 +38,25 @@ function App() {
           <Payment />
         </AmountContext.Provider>
         <input onInput={(e) => updateAmount(e.target.value)} />
-        <input
-          placeholder="enter your Username"
-          onInput={(e) => setUserName(e.target.value)}
-        />
+        {!userState.isUserCreated && (
+          <form>
+            <input
+              placeholder="enter your Username"
+              onInput={(e) =>
+                dispatch({ type: "SET_USERNAME", payload: e.target.value })
+              }
+            />
+            <input
+              placeholder="enter your money"
+              onInput={(e) =>
+                dispatch({ type: "SET_MONEY", payload: e.target.value })
+              }
+            />
+            <button type="button" onClick={saveUser}>
+              Generate User
+            </button>
+          </form>
+        )}
         <select onChange={(e) => updateCurrency(e.target.value)}>
           {Object.keys(CURRENCIES).map((currency) => (
             <option key={currency} value={currency}>
